ca0e1cf19a3d845d47855a315d26be0a60b7140b
srinivaspradhan64 <srinivaspradhan@gmail.com>
2023-11-22 18:08:24 -0500

    Add data and navbar menus - TODO add interface to pass from layout.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,7 @@
 'use client';
+import * as React from "react"
+import { cn } from "@/lib/utils"
 
-import { useState } from 'react';
 import { UserButton } from "@clerk/nextjs";
 import {
     NavigationMenu,
@@ -15,10 +16,37 @@ import {
 
 import Link from "next/link";
 
+import { Services, Documentation} from '@/utils/usernav';
 
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  )
+})
+ListItem.displayName = "ListItem"
 
 const NavBar = () => {
-    const [menuOpen, setmenuOpen] = useState(false);
+    const [menuOpen, setmenuOpen] = React.useState(false);
 
     const  handleNav = () => {
         setmenuOpen(!menuOpen)
@@ -36,17 +64,36 @@ const NavBar = () => {
                         <NavigationMenuList className="gap-5">
                             <NavigationMenuItem>
                                 <NavigationMenuTrigger className="font-mono bg-slate-200 hover:bg-slate-100 text-gray-600 hover:text-gray-900 text-md">Services</NavigationMenuTrigger>
-                                <NavigationMenuContent>1</NavigationMenuContent>
+                                <NavigationMenuContent>
+                                    <ul className="bg-slate-200 grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                                    {Services.map((service) => (
+                                        <ListItem
+                                            key={service.title}
+                                            title={service.title}
+                                            href={service.href}
+                                            >
+                                            {service.description}
+                                        </ListItem>
+                                    ))}
+                                    </ul>
+                                </NavigationMenuContent>
                             </NavigationMenuItem>
                             <NavigationMenuItem>
-                                <NavigationMenuTrigger className="font-mono bg-slate-200 hover:bg-slate-100 text-gray-600 hover:text-gray-900 text-md">Github</NavigationMenuTrigger>
-                                <NavigationMenuContent></NavigationMenuContent>
+                                <NavigationMenuTrigger className="font-mono bg-slate-200 hover:bg-slate-100 text-gray-600 hover:text-gray-900 text-md">Documentation</NavigationMenuTrigger>
+                                <NavigationMenuContent>
+                                    <ul className="bg-slate-200 grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+                                    {Documentation.map((doc) => (
+                                        <ListItem
+                                            key={doc.title}
+                                            title={doc.title}
+                                            href={doc.href}
+                                            >
+                                            {doc.description}
+                                        </ListItem>
+                                    ))}
+                                    </ul>
+                                </NavigationMenuContent>
                             </NavigationMenuItem>
-                            <Link href="/docs" legacyBehavior passHref>
-                                <NavigationMenuLink className="cursor-pointer font-mono text-gray-600 hover:text-gray-900 text-md">
-                                    Documentation
-                                </NavigationMenuLink>
-                            </Link>
                             <Link href="/faqs" legacyBehavior passHref>
                                 <NavigationMenuLink className="cursor-pointer font-mono text-gray-600 hover:text-gray-900 text-md">
                                     FAQs
--- utils/usernav.js
+++ utils/usernav.js
@@ -1,27 +1,61 @@
-const UserNav = [
-    {
-      alt: "BG1",
-      src: "/slider1_1.jpg"
-    },
-    {
-      alt: "BG2",
-      src: "/slider2.jpg"
-    },
-    {
-      alt: "BG3",
-      src: "/slider3.jpg"
-    },
-    {
-      alt: "BG4",
-      src: "/slider4.jpg"
-    },
-    {
-      alt: "BG5",
-      src: "/slider1.jpg"
-    }
+const Services = [
+  {
+    title: "AWS Lambda",
+    href: "/aws/lambda",
+    description: "Create AWS Lambda Function"
+  },
+  {
+    title: "AWS Secrets Manager",
+    href: "/aws/secrets",
+    description: "Create AWS Secrets Manager Secret."
+  },
+  {
+    title: "AWS Key Management Service",
+    href: "/aws/keys",
+    description: "Create AWS KMS Key and replicate to other supported regions."
+  },
+  {
+    title: "AWS Simple Notification Service",
+    href: "/aws/sns",
+    description: "Create AWS SNS Service."
+  },
+  {
+    title: "AWS DynamoDB",
+    href: "/aws/ddb",
+    description: "Create Dynamo Database in supported regions."
+  }
+]
+
+const Documentation = [
+  {
+    title: "AWS Lambda",
+    href: "https://docs.aws.amazon.com/lambda/?icmpid=docs_homepage_compute",
+    description: "Run code without thinking about servers."
+  },
+  {
+    title: "AWS Secrets Manager",
+    href: "https://docs.aws.amazon.com/secretsmanager/",
+    description: "AWS Secrets Manager helps you to securely encrypt, store, and retrieve credentials for your databases and other services."
+  },
+  {
+    title: "AWS Key Management Service",
+    href: "https://docs.aws.amazon.com/kms/latest/developerguide/overview.html",
+    description: "AWS Key Management Service (AWS KMS) is an encryption and key management service scaled for the cloud."
+  },
+  {
+    title: "AWS Simple Notification Service",
+    href: "https://docs.aws.amazon.com/kms/",
+    description: "Fully managed Pub/Sub service for A2A and A2P messaging."
+  },
+  {
+    title: "AWS DynamoDB",
+    href: "https://docs.aws.amazon.com/dynamodb/?icmpid=docs_homepage_featuredsvcs",
+    description: "Managed NoSQL database service."
+  }
 ]
 
 export {
-    UserNav
+    Services,
+    Documentation
 }
   
