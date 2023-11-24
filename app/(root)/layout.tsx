6e3d5ff4f22d9ccd65df4189a0c36165b3734bf3
srinivaspradhan64 <srinivaspradhan@gmail.com>
2023-11-23 22:28:39 -0500

    Update layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
               navbarstyle="bg-slate-200 shadow-sm flex h-16 items-center px-8"
               logo_styling="font-mono text-2xl font-semibold scale-150"
               dropdown="ml-auto flex flex-row items-center"
-              navbarspacing="gap-48 px-10"
+              navbarspacing="gap-48 px-24"
               dropdown_menustyling="font-mono bg-slate-200 hover:bg-slate-100 text-gray-600 hover:text-gray-900 text-md"
               navbaritemstyle="cursor-pointer font-mono text-gray-600 hover:text-gray-900 text-md"
               useraccountstyle="cursor-pointer font-mono text-gray-600 hover:text-gray-900 text-md"
