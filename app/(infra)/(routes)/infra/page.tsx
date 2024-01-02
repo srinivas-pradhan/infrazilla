3a36743ddbbf041127c2007099466a54b2d07f41
srinivaspradhan64 <srinivaspradhan@gmail.com>
2024-01-02 08:19:08 -0500

    Add Infra route and start work on Infra Modal.

---
+++ app/(infra)/(routes)/infra/page.tsx
@@ -0,0 +1,24 @@
+import { redirect } from 'next/navigation';
+import { auth } from '@clerk/nextjs';
+
+import Navbar from '@/components/navbar'
+
+
+export default function InfraLayout ({
+    children
+} : {
+    children: React.ReactNode
+  })
+  {
+    const { userId } = auth();
+
+    if (!userId) {
+      redirect('/sign-in');
+    }
+
+    return (
+        <>
+          {children}
+        </>
+    );
+}
---
+++ components/infra-modal.tsx
@@ -0,0 +1,3 @@
+'use client';
+
+import Modal from '@/components/ui/modal';
