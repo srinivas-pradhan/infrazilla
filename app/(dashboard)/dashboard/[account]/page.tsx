4005eb82cdf3bc95b6bb66e338e943958fb27157
srinivaspradhan64 <srinivaspradhan@gmail.com>
2024-01-12 17:03:23 -0500

    Add tailwind css classes and get card flipping to work

--- app/(dashboard)/dashboard/[account]/page.tsx
+++ app/(dashboard)/dashboard/[account]/page.tsx
@@ -45,9 +45,28 @@ export default function AccountPage ({
               useraccountstyle="cursor-pointer font-mono text-gray-600 hover:text-gray-900 text-md"
             />
             <div className=" grid grid-cols-3 gap-20 p-10">
-              <div>
+              <div className="bg-transparent cursor-pointer group perspective">
+                <div className="relative preserve-3d group-hover:my-rotate-y-180 w-full h-full duration-1000">
+                  <div className="absolute backface-hidden border-2 w-full h-full">
+                  <img src="/slider2.jpg" className="w-full h-full" />
+                  </div>
+                  <div className="absolute my-rotate-y-180 backface-hidden w-full h-full bg-gray-100 overflow-hidden">
+                    <div className="text-center flex flex-col items-center justify-center h-full text-gray-800 px-2 pb-24">
+                      <h1 className="text-3xl font-semibold">The King's Man</h1>
+                      <p className="my-2">9.0 Rating</p>
+                      <button
+                        className="bg-teal-500 px-6 py-2 font-semibold text-white rounded-full absolute -bottom-20 delay-500 duration-1000 group-hover:bottom-20 scale-0 group-hover:scale-125"
+                      >
+                        Watch Now
+                      </button>
+                    </div>
+                  </div>
+                </div>
+            </div> 
+    
+              {/* <div>
                 <AccountDetails/>
-              </div>
+              </div> */}
               <div>
                 <AccountDetails/>
               </div>
